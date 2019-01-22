f1d3d1f54bc30bc64aee6d2e46449b7d08c65c26
ankita0606 <praveenankita1967@gmail.com>
2019-01-22 22:54:44 +0530

    my first game in python

---
+++ game1.py
@@ -0,0 +1,35 @@
+print('Imagine you enter a dark room with two doors,door no.1 and door no.2 in front of you')
+print('''Door no.1 has a gaint bear eating cheese cake!!!!!
+	while Door no.2 have Trump and Kim Jong Un playing ludo!!!!''')
+a=int(input("the door no. you want to enter ="))
+if(a==1):
+	print('''===>what can you do now?????
+		        a.Take the cake?
+		        b.Scream at the bear!!
+		        c.none of these''')
+	d=input('choose your option!!!')
+	if(d=='a'):
+	      	print('What!!! you took his cheesy cake......you are surely going to die by the time you read this')
+	elif(d=='b'):
+	      	print('Oh god!!Bears don\'t like people who shout at them......you are surely going to be the topping of his cake')
+	elif(d=='c'):
+	      	print('Omg!!!!!!!!! Your fate totally depends on the taste of the cake.....pray till the Bear completes his desert!!!!')
+	else:
+	      	print('Hey!! You have only three options...')
+elif(a==2):
+	print('''===>What can you do now????
+		        a.Take autograph!
+		        b.Join the game with them!
+		        c.shoot both of them!!''')
+	e=input('choose your option!!!')
+	if(e=='a'):
+	      	print('Be carefull whose autograph you ask first!!!!......Both gonna try there nuclear weapon over you!!!')
+	elif(e=='b'):
+	      	print('oh!!!!!!!!!! Hope you don\'t win or the rest will tomorrows headlines!!!')
+	elif(e=='c'):
+	      	print('hurray!!! you saved the world from world war 3')
+	else:
+	      	print('Hey!! You have only three options...')
+else:
+	print('Oops!! You have only two choices.....Choose between them!!')
+
